d013dab47a7c8556ace7106183ca00005a75351c
Kyriden <matteo.clavel2@gmail.com>
2020-03-15 19:41:06 +0100

    addCrous

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,19 +2,20 @@ import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import { withAutorization } from './Autorization';
 import caddie from '../img/supermarket.svg';
+import crous from '../img/crous.png';
 
 class Navigation extends Component {
     render() {
         return(
             <div className='header'>
                 <ul className='navigation'>
+                    <img src={crous} className='crous' alt='IUT vétuste'/>
                     <li> <Link to='/'> Home </Link> </li>
                     {this.props.isLogged ? '' : <li> <Link to='/signin'> Sign in </Link></li>}
                     {this.props.isLogged ? '' : <li> <Link to='/login'> Login </Link></li>}
                     {this.props.isLogged ? <li> <Link to='/' onClick={this.props.toLogout}> Log out </Link></li> : '' }
                     <span class='panier'> <Link to='/'>  <img src={caddie} className="caddie" alt="logo" /> {this.props.articles} </Link> </span> 
                 </ul>
-                
             </div>
         );
     }
